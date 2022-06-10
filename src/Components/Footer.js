a3a7e2e96804fa4718c7c18ff9081399faf15025
Dhruv132k <2013dhruv@gmail.com>
2022-06-10 12:10:15 +0530

    Please check the updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,103 +2,124 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className="container-fluid ">
-            <div className="row footer-one text-start h-auto">
-                <div className="col-md-2">
-                    <font className="footer-headings"> ABOUT WEBSITE</font>
-                    <ul>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>About Us</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Copyright Statement</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Hyperlink Policy</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Content Management Policy</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Privacy Policy</font>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-md-2">
-                    <font className="footer-headings"> QUICK LINKS</font>
-                    <ul>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Recruitment</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Tenders</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Help</font>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-md-2">
-                    <font className="footer-headings"> REACH US</font>
-                    <ul>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Contact Us</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Feedback</font>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-md-2">
-                    <font className="footer-headings"> NAVIGATE SIGHT</font>
-                    <ul>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Home</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Search</font>
-                            </a>
-                        </li>
-                        <li id="footer-sub-headings">
-                            <a href="/">
-                                <font>Sitemap</font>
-                            </a>
-                        </li>
-                    </ul>
+        <>
+            <div className="container-fluid">
+                <div className="row footer-one text-start h-auto justify-content-center">
+                    <div className="col-md-2 text-left">
+                        <font className="footer-headings"> ABOUT WEBSITE</font>
+                        <ul>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>About Us</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Copyright Statement</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Hyperlink Policy</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Content Management Policy</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Privacy Policy</font>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-2">
+                        <font className="footer-headings"> QUICK LINKS</font>
+                        <ul>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Recruitment</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Tenders</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Help</font>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-2">
+                        <font className="footer-headings"> REACH US</font>
+                        <ul>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Contact Us</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Feedback</font>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-2">
+                        <font className="footer-headings"> NAVIGATE SIGHT</font>
+                        <ul>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Home</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Search</font>
+                                </a>
+                            </li>
+                            <li id="footer-sub-headings">
+                                <a href="/">
+                                    <font>Sitemap</font>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-2">
+                        <div id="digital-india">
+                            <img src="http://lgpunjab.gov.in/eSewa/gurdaspur/wp-content/uploads/sites/302/2017/10/digital-india-300x114.png" alt="" srcset="http://lgpunjab.gov.in/eSewa/gurdaspur/wp-content/uploads/sites/302/2017/10/digital-india-300x114.png 300w, http://lgpunjab.gov.in/eSewa/gurdaspur/wp-content/uploads/sites/302/2017/10/digital-india.png 325w" />
+                        </div>
+                        <div id="visitors">
+                            <h4>
+                                <font>VISITORS</font>
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis aliquid provident non deserunt voluptatibus quibusdam labore rerum delectus atque suscipit dolores quidem reiciendis similique, tenetur laborum vero, ut magnam ullam accusamus ducimus, omnis nulla!
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-
-
-
-
-
-            {/* <div className="row footer-two">
-
-            </div> */}
-        </div>
+            <div className="container-fluid">
+                <div className="row footer-two h-auto justify-content-center">
+                    <div className="col-md-8">
+                        <div id='random-one'>
+                            <p className="pull-left">
+                                <font id="copyright">Copyright 2022 © Gurdaspur. All Rights Reserved </font>
+                            </p>
+                            <p className="pull-right">
+                                <img src="http://lgpunjab.gov.in/eSewa/wp-content/uploads/sites/302/2018/04/digit-logo-footer.png"/>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
