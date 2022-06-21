88b0ce13cf2b93683120e7f1a9ee3b1dffa85a33
Ramesh Kumar <erramesh000@gmail.com>
2022-06-21 09:54:23 +0530

    Updated router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 
 import './App.css';
+
 import Rednavbar from './Components/Rednavbar';
 import Helplinebar from './Components/Helplinebar';
 import Covid from './Components/Covid';
-import 'D:/Web Dev/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import $ from 'jquery';
 import Bluenavbar from './Components/Bluenavbar';
 import Footer from './Components/Footer';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import One from './Components/demo/One';
+import Two from './Components/demo/Two';
+import Error from './Components/demo/Error';
 
-function App() {
+const App = () => {
   return (
-    <>
+    
+   <div className="app">
       <Rednavbar/>
       <Helplinebar/>
       <Covid/>
       <Bluenavbar/>
+      <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<One />} />
+  <Route path="/Two" element={<Two />} />  
+  <Route path="*" element={<Error />} />
+ </Routes>
+ </BrowserRouter>
+ <section>
+        
+        </section>
       <Footer/>
-    </>
+    </div>
+    
   );
 }
 
